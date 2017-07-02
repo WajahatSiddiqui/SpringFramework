d319b933b5f780bfdccc934aa3c1054b60c7bddf
Wajahat <wajahat.siddiqui22@gmail.com>
2017-07-02 18:27:55 +0530

    Using ApplicationContext and Property Initialization for a class
    Signed-off-by: Wajahat <wajahat.siddiqui22@gmail.com>

--- SpringCore/src/com/wajahat/learn/spring/core/DrawingApp.java
+++ SpringCore/src/com/wajahat/learn/spring/core/DrawingApp.java
@@ -1,16 +1,18 @@
 package com.wajahat.learn.spring.core;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
 		
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle triangle = (Triangle) factory.getBean("triangle");
+		/*BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle = (Triangle) factory.getBean("triangle");*/
 		
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+				
 		triangle.draw();
 
 	}
--- SpringCore/src/com/wajahat/learn/spring/core/Triangle.java
+++ SpringCore/src/com/wajahat/learn/spring/core/Triangle.java
@@ -1,9 +1,21 @@
 package com.wajahat.learn.spring.core;
 
 public class Triangle {
-	
+
+	private String type;
+
+
+
 	public void draw() {
-		System.out.println("Triangle Drawn");
+		System.out.println(getType() + " Triangle Drawn");
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }
